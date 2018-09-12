90ab94e44733d3b07e49c6019cad7a3a08d966eb
Paul Pogonyshev <pogonyshev@gmail.com>
2018-09-12 22:23:42 +0200

    Fix `generate-extmaps.sh' to run properly when run from a different directory.

--- generate-extmaps.sh
+++ generate-extmaps.sh
@@ -3,6 +3,7 @@
 set -e
 
 OWN_DIRECTORY=$(dirname $0)
+cd $OWN_DIRECTORY
 
 if [ -z "$EMACS" ] ; then
     EMACS=emacs
@@ -16,7 +17,6 @@ cd ..
 java -cp dev HarvestData --locales | tr "\n" " "                                                                                \
   | $EMACS -batch                                                                                                               \
           --eval "(progn (require 'package) (package-initialize))"                                                              \
-          --directory "$OWN_DIRECTORY"                                                                                          \
           --eval "(when (locate-file \"local-environment.el\" (list (car load-path))) (load \"local-environment.el\" nil t t))" \
           --eval "(require 'extmap)"                                                                                            \
           --eval "(extmap-from-alist \"locale-data.extmap\" (read-minibuffer \"\") :overwrite t)"
@@ -24,7 +24,6 @@ java -cp dev HarvestData --locales | tr "\n" " "                                
 java -cp dev HarvestData --timezones | tr "\n" " "                                                                              \
   | $EMACS -batch                                                                                                               \
           --eval "(progn (require 'package) (package-initialize))"                                                              \
-          --directory "$OWN_DIRECTORY"                                                                                          \
           --eval "(when (locate-file \"local-environment.el\" (list (car load-path))) (load \"local-environment.el\" nil t t))" \
           --eval "(require 'extmap)"                                                                                            \
           --eval "(extmap-from-alist \"timezone-data.extmap\" (read-minibuffer \"\") :overwrite t)"

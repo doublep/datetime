a71c4abd7993ab207fc33527f4d9aa09d116a0fb
Paul Pogonyshev <pogonyshev@gmail.com>
2024-03-31 22:24:44 +0200

    Post-release version bump.

--- datetime.el
+++ datetime.el
@@ -4,7 +4,7 @@
 
 ;; Author:     Paul Pogonyshev <pogonyshev@gmail.com>
 ;; Maintainer: Paul Pogonyshev <pogonyshev@gmail.com>
-;; Version:    0.10
+;; Version:    0.10.1snapshot
 ;; Keywords:   lisp, i18n
 ;; Homepage:   https://github.com/doublep/datetime
 ;; Package-Requires: ((emacs "25.1") (extmap "1.1.1"))
